f75a12127899af3c12253cbe278d52b9fc84e59d
its-MAHADi <mhkhandakar57@gmail.com>
2025-03-02 05:21:43 +0600

    A5-Dev-Board assignment done

--- Devboard.js
+++ Devboard.js
@@ -8,29 +8,50 @@ document.getElementById("theme-btn").addEventListener("click",function getColour
    document.getElementById("theme-btn").innerHTML = color;
 })
 
-function disableBtn() {
-    document.getElementById("btn-complete").disabled = true;
-}
-function disableBtn1() {
-    document.getElementById("btn-complete1").disabled = true;
-}
-function disableBtn2() {
-    document.getElementById("btn-complete2").disabled = true;
-}
-function disableBtn3() {
-    document.getElementById("btn-complete3").disabled = true;
-}
-function disableBtn4() {
-    document.getElementById("btn-complete4").disabled = true;
-}
-function disableBtn5() {
-    document.getElementById("btn-complete5").disabled = true;
+
+const checkboxCount = document.getElementById("checkbox-count");
+const taskAssignedCount = document.getElementById("task-assigned-count");
+const clearHistoryButton = document.getElementById("clear-history-button");
+const taskCompletedButton = document.querySelectorAll(".task-completed-button");
+const mainRightBottomContainer = document.getElementById("main-right-bottom-container");
+const todayDate = document.getElementById("today-date");
+
+let currentDate = new Date().toLocaleDateString("en-US", { weekday: 'short', month: 'short', day: '2-digit', year: 'numeric' });
+todayDate.innerText = currentDate;
+let taskAssignedCountValue = 6;    
+let checkboxCountValue = 23;
+
+
+for(let i = 0 ; i < taskCompletedButton.length ; i++){
+    taskCompletedButton[i].addEventListener("click",function(event){
+        alert("Board updated Successfully")
+        event.target.classList.add("disabled-button");
+        event.target.setAttribute("disabled",true);
+        
+        taskAssignedCountValue = taskAssignedCountValue-1;
+        taskAssignedCount.innerText = taskAssignedCountValue;
+        
+        checkboxCountValue = checkboxCountValue+1;
+        checkboxCount.innerText = checkboxCountValue;
+
+        let taskCompletedItemName = event.target.parentNode.childNodes[1].innerText
+        let currentTime = new Date().toLocaleTimeString("en-US", { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+
+        let div = document.createElement("div");
+        div.innerHTML=`
+        <p>You have Complete The Task ${taskCompletedItemName} at ${currentTime}</p>
+        `;
+        div.classList.add("history-box")
+        mainRightBottomContainer.appendChild(div);
+        
+        if(checkboxCountValue > 28){
+            alert("congrates!!! You have completed all the current task")
+        }
+    })
 }
 
-// document.getElementById("btn-complete").addEventListener("click", function(){
-   
-// })
-
-document.getElementById("btn-complete").addEventListener("click",function(){
-    console.log("hello")
+clearHistoryButton.addEventListener("click",function(){
+    mainRightBottomContainer.innerHTML = ""
 })
+
+// done js
